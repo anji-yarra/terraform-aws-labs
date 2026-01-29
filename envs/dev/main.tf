f306d0dbda40de296b1d405f7e0e596fef7fb328
tidingdbruh <tidingdbruh@gmail.com>
2026-01-29 14:23:21 +0530

    Add SG and attach to EC2

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -19,6 +19,28 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
+#Security group 
+
+resource "aws_security_group" "web_sg"{
+  name = "terraform-dev-sg"
+  description = "Allow SSH access"
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"   # t2.micro is NOT available in eu-north-1
